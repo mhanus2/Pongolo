7b0352088720ef51ad155c86cff48087b924f78c
mhanus2 <mhmartinhanus@gmail.com>
2024-05-24 18:17:28 +0200

    Added: Paddle

---
+++ src/pongolo/Paddle.java
@@ -0,0 +1,35 @@
+package pongolo;
+
+public class Paddle {
+    private float angle;
+    private float rotationSpeed;
+    private static final float WIDTH = (float) Math.PI / 8;
+    private static final float MAX_ROTATION_SPEED = 0.02f;
+
+    public Paddle(float initialAngle) {
+        this.angle = initialAngle;
+        this.rotationSpeed = 0.0f;
+    }
+
+    public void updateAngle() {
+        angle += rotationSpeed;
+        if (angle < 0) angle += (float) (2 * Math.PI);
+        if (angle >= 2 * Math.PI) angle -= (float) (2 * Math.PI);
+    }
+
+    public void setRotationSpeed(float rotationSpeed) {
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public static float getWidth() {
+        return WIDTH;
+    }
+
+    public static float getMaxRotationSpeed() {
+        return MAX_ROTATION_SPEED;
+    }
+}
