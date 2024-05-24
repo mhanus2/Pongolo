2379ff96f4759c6afa4013e276ab31c5cb1b1c7f
mhanus2 <mhmartinhanus@gmail.com>
2024-05-24 18:15:41 +0200

    Added: Basic classes

---
+++ src/pongolo/App.java
@@ -0,0 +1,13 @@
+package pongolo;
+
+import pongolo.global.LwjglWindow;
+
+
+
+public class App {
+
+    public static void main(String[] args) {
+        new LwjglWindow(new Renderer(), false);
+    }
+
+}
