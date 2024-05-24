e747742273241f534bed2e9ac23101a91f66743e
mhanus2 <mhmartinhanus@gmail.com>
2024-05-24 18:16:38 +0200

    Added: Ball

---
+++ src/pongolo/Ball.java
@@ -0,0 +1,47 @@
+package pongolo;
+
+public class Ball {
+    private float x, y;
+    private float dirX, dirY;
+    private float speed;
+    private static final float RADIUS = 0.03f;
+
+    public Ball(float x, float y, float dirX, float dirY, float speed) {
+        this.x = x;
+        this.y = y;
+        this.dirX = dirX;
+        this.dirY = dirY;
+        this.speed = speed;
+    }
+
+    public void updatePosition() {
+        x += dirX * speed;
+        y += dirY * speed;
+    }
+
+    public void reflect(float normalX, float normalY) {
+        float dotProduct = dirX * normalX + dirY * normalY;
+        dirX -= 2 * dotProduct * normalX;
+        dirY -= 2 * dotProduct * normalY;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public static float getRadius() {
+        return RADIUS;
+    }
+
+    public void increaseSpeed(float increment) {
+        speed += increment;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+}
